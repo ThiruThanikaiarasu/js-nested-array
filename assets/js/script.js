c93cd043ecd6702f5a8e534f503a661f1ea0430d
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-03-21 21:50:29 +0530

    Add. IsNested function added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,47 @@
-// this is script 
+let array1 = [3,1]
+let array2 = [0,4]
+
+// IsNested(array1,array2)
+
+console.log(IsNested(array1,array2))
+
+function IsNested(array1,array2)
+{
+    let min_value_array1 = 9999;
+    let max_value_array1 = 0;
+    let min_value_array2 = 9999;
+    let max_value_array2 = 0;
+    
+        minValue = Infinity;
+        maxValue = -Infinity;
+      
+        for (item of array1) {
+            // find minimum value
+            if (item < min_value_array1)
+                min_value_array1 = item;
+      
+            // find maximum value
+            if (item > max_value_array1)
+                max_value_array1 = item;
+        }
+
+        for (item of array2) {
+            // find minimum value
+            if (item < min_value_array2)
+                min_value_array2 = item;
+      
+            // find maximum value
+            if (item > max_value_array2)
+                max_value_array2 = item;
+        }
+
+        console.log(min_value_array1)
+        console.log(max_value_array1)
+        console.log(min_value_array2)
+        console.log(max_value_array2)
+    
+
+    return (min_value_array1>min_value_array2)&&(max_value_array1<max_value_array2);
+
+}
+
